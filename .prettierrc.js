cbb914ab0730d02220999044e531b429d97d7553
Lucas Abroms <laabroms@gmail.com>
2023-02-19 17:18:40 -0800

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/* globals module */
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 140,
+  tabWidth: 2,
+  endOfLine: 'auto',
+};
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider {...{ router }} />
+  </React.StrictMode>,
+);
