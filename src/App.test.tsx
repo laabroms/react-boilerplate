1e918b727283c41737622c81acb32f0389e6d727
Lucas Abroms <laabroms@gmail.com>
2023-02-19 17:33:26 -0800

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 
-import App from '../src/App';
+import App from './App';
 
 describe('App', () => {
   it('renders headline', () => {
